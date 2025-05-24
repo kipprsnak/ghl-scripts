795c38133928d2a22c969ecc2e737dc783ffe81e
kipprsnak <kipprsnak@gmail.com>
2025-05-24 11:56:21 -0700

    Update products.js

--- netlify/functions/products.js
+++ netlify/functions/products.js
@@ -1,12 +1,9 @@
-exports.handler = async function (event, context) {
-  const GHL_API_KEY = process.env.GHL_API_KEY;
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      GHL_API_KEY_PRESENT: !!GHL_API_KEY,
-      GHL_API_KEY_START: GHL_API_KEY ? GHL_API_KEY.substring(0, 20) : 'undefined',
-      GHL_API_KEY_LENGTH: GHL_API_KEY ? GHL_API_KEY.length : 0
-    })
-  };
-};
+const response = await fetch('https://rest.gohighlevel.com/v1/products', {
+  method: 'GET',
+  headers: {
+    Authorization: GHL_API_KEY,
+    'Content-Type': 'application/json',
+    Accept: 'application/json',
+    Version: '2021-07-28'
+  }
+});
